35aaaccda07095755e32321faec5346a58689c49
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-19 20:28:16 +0200

    added React Bootstrap + modal

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::GifsController < ApplicationController
 
   def index
     @gifs = Gif.all
+    render json: @gifs
   end
 
   def show
--- app/javascript/simpsons/components/Gif.js
+++ app/javascript/simpsons/components/Gif.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InfoModal from './InfoModal'
 import clsx from "clsx"
 
 const Gif = (props) => {
@@ -8,21 +9,38 @@ const Gif = (props) => {
     "d-none": props.leftPanel})
 
 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   return (
-    <div className="card my-card">
-        <div className="d-flex flex-column justify-content-end">
-          <img src={gif.url} className="card-img-top my-card-image" alt="gif" />
-          <div className="card-body d-flex flex-column">
-            <h5 className="card-title text-center">{gif.title}</h5>
-            <button
-              onClick={() => setSelectedGifId(gif.id)}
-              className={myButtonClass}
-            >Select
-            </button>
+      <>
+        <div className="card my-card">
+          <div className="d-flex flex-column justify-content-end">
+            <img src={gif.url} className="card-img-top my-card-image" alt="gif" />
+            <div className="card-body d-flex flex-column">
+              <h5 className="card-title text-center">{gif.title}</h5>
+              <button
+                onClick={() => {
+                  setSelectedGifId(gif.id)
+                  handleShow()
+                }}
+                className={myButtonClass}
+              >Select
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-  )
+
+        <InfoModal
+          size="lg"
+          gif={gif}
+          handleClose={handleClose}
+          show={show}/>
+      </>
+   )
+
 }
 
 export default Gif
